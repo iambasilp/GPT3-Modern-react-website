249b79d4fec95b61447a60afe6d08aac4b274869
iambasilp <basilxg321@gmail.com>
2023-01-17 20:20:20 +0530

    File and Folder structure finally completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import { useState } from 'react'
 import './App.css'
 
 import { Article, Brand, CTA, Feature, Navbar } from './components'
-import {Blog, Features, Footer, Possibility, WhatGPT3} from './container'
+import {Blog, Header, Features, Footer, Possibility, WhatGPT3} from './container'
 function App() {
   
   return (
     <div className="App">
-      
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   )
 }
